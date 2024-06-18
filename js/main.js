72414c2ec4162cb816c210ec86da386860df6d1c
battistaennio <ennio.battista99@gmail.com>
2024-06-18 14:50:24 +0200

    feat: create team members object array

--- js/main.js
+++ js/main.js
@@ -1,3 +1,38 @@
 //Creare l’array di oggetti con le informazioni fornite.
 //Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
 //Stampare le stesse informazioni su DOM sottoforma di stringhe
+
+
+//creo l'array con i dati dei componenti del team
+const teamMembers = [
+    {
+        "name": "Wayne Barnett",
+        "role": "Founder & CEO",
+        "image": "wayne-barnett-founder-ceo",
+    },  
+    {
+        "name": "Angela Caroll",
+        "role": "Chief Editor",
+        "image": "angela-caroll-chief-editor",
+    },
+    {
+        "name": "Walter Gordon",
+        "role": "Office Manager",
+        "image": "walter-gordon-office-manager",
+    },
+    {
+        "name": "Angela Lopez",
+        "role": "Social Media Manager",
+        "image": "angela-lopez-social-media-manager",
+    },
+    {
+        "name": "Scott Estrada",
+        "role": "Developer",
+        "image": "scott-estrada-developer",
+    },
+    {
+        "name": "Barbara Ramos",
+        "role": "Graphic Designer",
+        "image": "barbara-ramos-graphic-designer",
+    },
+];
